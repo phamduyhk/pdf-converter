ac22cf9e7eb7c389d88e1f19e6a01c200752a3ad
pham duy <phamduyhk@gmail.com>
2019-10-11 22:45:04 +0900

    release

--- Client/src/app/uploadFileComponent/uploadFile.component.js
+++ Client/src/app/uploadFileComponent/uploadFile.component.js
@@ -42,12 +42,14 @@
             // $ctrl.langs = ['eng', 'vie', 'jpn'];
             $ctrl.ocrResult = "";
             $ctrl.progressCircular = false;
+            $ctrl.getTextField = false;
             $ctrl.voiceSrc = "";
         }
 
         $ctrl.submit = function() { //function to call on form submit
             $ctrl.ocrResult = "";
             $ctrl.progressCircular = true;
+            $ctrl.getTextField = true;
             console.log($ctrl.file);
             if ($ctrl.file) { //check if from is valid
                 $ctrl.upload($ctrl.file); //call upload function
@@ -70,39 +72,14 @@
                 if (resp.data.error_code === 0) { //validate success
                     // show alert
                     // $window.alert('Success ' + resp.config.data.file.name + 'uploaded. Response: ');
-                    var type = $ctrl.file.type;
+                    $ctrl.type = $ctrl.file.type;
                     $ctrl.outUrl = resp.data.msg;
                     console.log(resp);
-                    console.log($ctrl.outUrl);
-                    if (type == "application/pdf") {
-                        console.log("get text from PDF");
-                        $http.get('/api/pdf2text', {
-                            params: {
-                                url: $ctrl.outUrl,
-                                lang: $ctrl.lang.tesseract
-                            }
-                        }).then(function(response) {
-                            console.log(response);
-                            $ctrl.progressCircular = false;
-                            $ctrl.ocrResult = response.data;
-                        })
-
-                    } else {
-                        console.log("get OCR result from IMG");
-                        $http.get('/api/ocr', {
-                            params: {
-                                url: $ctrl.outUrl,
-                                lang: $ctrl.lang.tesseract
-                            }
-                        }).then(function(response) {
-                            console.log(response);
-                            $ctrl.progressCircular = false;
-                            $ctrl.ocrResult = response.data;
-                        });
-                    }
-
+                    console.log({outurl:$ctrl.outUrl});
+                    $ctrl.progressCircular = false;
                 } else {
                     $window.alert('an error occured');
+                    $ctrl.progressCircular = false;
                 }
             }, function(resp) { //catch error
                 console.log('Error status: ' + resp.status);
@@ -115,6 +92,40 @@
             });
         };
 
+
+        $ctrl.getText = function(pagenumber){
+            if ($ctrl.type == "application/pdf") {
+                        console.log("get text from PDF");
+                        $ctrl.ocrResult = "";
+                        var requestBody =  {
+                            url: $ctrl.outUrl,
+                            lang: $ctrl.lang.tesseract,
+                            startpage: pagenumber
+                        }
+                        console.log({requestBody:requestBody});
+                        $http.get('/api/pdf2text', {
+                            params: requestBody
+                        }).then(function(response) {
+                            console.log(response);
+                            // $ctrl.progressCircular = false;
+                            $ctrl.ocrResult = response.data;
+                        })
+
+                    } else {
+                        console.log("get OCR result from IMG");
+                        $http.get('/api/ocr', {
+                            params: {
+                                url: $ctrl.outUrl,
+                                lang: $ctrl.lang.tesseract
+                            }
+                        }).then(function(response) {
+                            console.log(response);
+                            // $ctrl.progressCircular = false;
+                            $ctrl.ocrResult = response.data;
+                        });
+                    }
+        }
+
         $ctrl.getVoice = function(text) {
             console.log("get voice from google");
             console.log({
@@ -122,13 +133,19 @@
                 lang: $ctrl.lang.googletranslate,
                 filename: $ctrl.fileName
             })
-            $http.get('/api/voice', {
-                params: {
-                    text: text,
-                    lang: $ctrl.lang.googletranslate,
-                    filename: $ctrl.fileName
-                }
-            }).then(function(response) {
+            var tts_url = 'http://translate.google.com/translate_tts?ie=UTF-8&q='+text.slice(0,900)+
+            '&tl='+$ctrl.lang.googletranslate+
+            '&total=1&idx=0&textlen=1000&client=tw-ob'; //&ttsspeed=1';
+
+            //  {
+            //      params: {
+            //          text: text,
+            //          lang: $ctrl.lang.googletranslate,
+            //          filename: $ctrl.fileName
+            //      }
+            //  }
+
+            $http.get(tts_url).then(function(response) {
                 console.log(response);
                 var filename = response.data.slice(1, response.data.length);
                 var url = "./../../public/voice" + filename;
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # pdf-converter
+**HOW TO RUN**
+`gulp app`  
+`gulp vendor`  
+`cd server`  
+`node app.js`
+
+link: localhost/src/app
